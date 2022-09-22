e56ef1f7a00467cb3feb27e85c844eb17806095b
chmel999 <chmel999@rambler.ru>
2022-09-23 02:30:43 +0300

    Initial

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,17 @@
 package org.example;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.springframework.http.*;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        try {
+
             System.out.println("Hello world!");
             RestTemplate restTemplate = new RestTemplate();
             String url = "http://94.198.50.185:7081/api/users";
@@ -26,15 +22,37 @@ public class Main {
             restTemplate.setMessageConverters(messageConverters);
             ResponseEntity<User[]> response = restTemplate.getForEntity(url, User[].class);
             System.out.println(response.getStatusCode());
-            System.out.println(response.getStatusCodeValue());
             for (User user: response.getBody()) {
                 System.out.println(user);
             }
-            System.out.println(response.getBody());
             String cookie = response.getHeaders().get("Set-Cookie").get(0);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        System.out.println(cookie.split(";")[0]);
+            User user = new User();
+            user.setId(3l);
+            user.setName("James");
+            user.setLastName("Brown");
+            user.setAge(21);
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Cookie", cookie);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+            Map<String, Object> map = new HashMap<>();
+            map.put("name", "James");
+            map.put("lastName", "Brown");
+            map.put("age", "21");
+        HttpEntity<User> entity = new HttpEntity<>(user, headers);
+            RestTemplate restTemplate2 = new RestTemplate();
+        ResponseEntity<String>     response2 = restTemplate2.postForEntity(url, entity, String.class);
+        System.out.println(response2.getBody());
+        user.setName("Thomas");
+        user.setLastName("Shelby");
+
+
+        ResponseEntity<String>     response3 = restTemplate2.exchange(url, HttpMethod.PUT, entity, String.class);
+        System.out.println(response3.getBody());
+
+        HttpEntity<String> entity2 = new HttpEntity<>(headers);
+        ResponseEntity<String>     response4 = restTemplate2.exchange(url + "/3", HttpMethod.DELETE, entity2, String.class);
+        System.out.println(response4.getBody());
 
     }
 }
