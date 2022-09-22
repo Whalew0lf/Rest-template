86298f2327128a8d57622a4a329009e252d3d564
Whalew0lf <777chmel777@gmail.com>
2022-09-22 19:41:09 +0300

    initial

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,40 @@
 package org.example;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        RestTemplate restTemplate = new RestTemplate();
-        String url = "http://94.198.50.185:7081/api/users";
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-        System.out.println(response.getStatusCode());
-        System.out.println(response.getStatusCodeValue());
-        System.out.println(response.getBody());
-        String coockie = response.getHeaders().get("Set-Cookie").get(0);
-        ResponseEntity<String> response1 restTemplate.
+        try {
+            System.out.println("Hello world!");
+            RestTemplate restTemplate = new RestTemplate();
+            String url = "http://94.198.50.185:7081/api/users";
+            MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+            converter.setSupportedMediaTypes(Collections.singletonList(MediaType.ALL));
+            List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
+            messageConverters.add(converter);
+            restTemplate.setMessageConverters(messageConverters);
+            ResponseEntity<User[]> response = restTemplate.getForEntity(url, User[].class);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getStatusCodeValue());
+            for (User user: response.getBody()) {
+                System.out.println(user);
+            }
+            System.out.println(response.getBody());
+            String cookie = response.getHeaders().get("Set-Cookie").get(0);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,50 @@
+package org.example;
+
+public class User {
+    long id;
+    String name;
+    String lastName;
+    int age;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
